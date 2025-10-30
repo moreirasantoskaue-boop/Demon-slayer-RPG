42f14f1b9a2650f7d6b0a1b72a7b81e258d76f69
moreirasantoskaue-boop <moreira.santos.kaue@escola.pr.gov.br>
2025-10-30 09:25:24 -0300

    aaaaaaaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const avanca = document.querySelectorAll("btn-proximo")
 console.log(avanca)
 
 avanca.forEach(button => {
-button.addEventListener
+button.addEventListener('click',function){
+    const atual = document.querySelector('ativo')
+    const proximopasso = "passo-" + this.getAttribute('data-proximo')
+    atual.classList.remove('ativo')
+    document.getElementById(proximopasso).classList.add('ativo')
+})
+
+
+
+}
 
 
 })
